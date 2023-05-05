0e51a82b3485baa582f597e6d4aa3eb644bc8838
AnnaFutryk <anna.futryk@gmail.com>
2023-05-05 10:04:36 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,22 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const refs = {
     galleryList: document.querySelector('.gallery'),
-    
 }
 
-// const galleryItem = galleryItems.map(({ preview, original, description }) =>
-//     `<li class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </li>`).join('');
-
-// galleryList.insertAdjacentHTML('beforeend', galleryItem);
 const listItemsMarkup = createListItemsMarkup(galleryItems);
 
 refs.galleryList.innerHTML = listItemsMarkup;
@@ -36,4 +22,26 @@ function createListItemsMarkup(items) {
 </li>`).join('');
 }
 
+refs.galleryList.addEventListener('click', imageClickOpen);
+
+function imageClickOpen(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    // модалка через бібліотеку//
+    const modal = basicLightbox.create(
+        `<img src = "${event.target.dataset.source}" width="800" height="600">`
+    );
+    modal.show();
+  
+    window.addEventListener('keydown', closeModal);
+
+    function closeModal(event) {
+        if (event.code === 'Escape') {
+            modal.close()
+        };
+    };
+}
+    
 
