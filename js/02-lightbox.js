43fa69d21574792521ea0047b1fe1722b904aee0
AnnaFutryk <anna.futryk@gmail.com>
2023-05-05 10:04:39 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    galleryList: document.querySelector('.gallery'),
+}
 
-console.log(galleryItems);
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+
+refs.galleryList.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+    return items.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`).join('');
+};
+
+refs.galleryList.addEventListener('click', imageClickOpen);
+
+function imageClickOpen(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = new SimpleLightbox('.gallery__link', {
+        sourceAttr: 'href',
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+};
